97427bf3f7d81a5bacea9573f332c4a946261e83
david <davidweisspost@gmail.com>
2019-09-13 09:51:22 +0200

    Draw bipartite graph

--- create-csv-graph.py
+++ create-csv-graph.py
@@ -1,11 +1,14 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-data = open("graph.csv", "r")
+data = open("simple-query.csv", "r")
+next(data, None)
 
+simpleGraph = nx.parse_edgelist(data, delimiter=",")
 
-csvGraph = nx.parse_edgelist(data)
+leftSide = nx.bipartite.sets(simpleGraph)[0]
 
+bipartiteLayout = nx.bipartite_layout(simpleGraph, leftSide, aspect_ratio=0.5, scale=0.2)
 
-nx.draw(csvGraph, with_labels=True)
+nx.draw_networkx(simpleGraph, bipartiteLayout, with_labels=True, font_size=10, edge_color="grey")
 plt.show()
