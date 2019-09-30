f6a85d235e420481e9189067ae8266be7e8cd93f
David Weiß <davidweisspost@gmail.com>
2019-09-30 17:59:13 +0200

    Add simple graph

--- create-csv-graph.py
+++ create-csv-graph.py
@@ -1,15 +1,23 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-data = open("simple-query.csv", "r")
-next(data, None) # Skip csv header
+# data = open("simple-query.csv", "r")
+# next(data, None) # Skip csv header
 
-G = nx.parse_edgelist(data, delimiter=",")
+# G = nx.parse_edgelist(data, delimiter=",")
+
+G = nx.Graph()
+edgeList = [(1, 6), (1, 7), (1, 8),
+            (2, 6), (2, 7), (2, 8),
+            (3, 6), (3, 7), (3, 8), (3, 9),
+            (4, 7), (4, 8),
+            (5, 9)]
+G.add_edges_from(edgeList)
 
 bipartition_1 = nx.bipartite.sets(G)[0]
 bipartition_2 = nx.bipartite.sets(G)[1]
 
-if len(bipartition_1) > len(bipartition_2):
+if len(bipartition_1) < len(bipartition_2):
     V = bipartition_1
     U = bipartition_2
 else:
@@ -42,7 +50,7 @@ print("RC", RC)
 # bipartiteLayout = nx.bipartite_layout(G, U, aspect_ratio=0.5, scale=0.2)
 # nx.draw_networkx(G, bipartiteLayout, with_labels=True, font_size=10, edge_color="grey")
 
-# nx.draw_networkx(G_S, nx.circular_layout(G_S))
-# nx.draw_networkx_edge_labels(G_S, nx.circular_layout(G_S))
+nx.draw_networkx(G_S, nx.circular_layout(G_S))
+nx.draw_networkx_edge_labels(G_S, nx.circular_layout(G_S))
 
-# plt.show()
+plt.show()
