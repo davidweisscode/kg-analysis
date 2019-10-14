aca07b695bdc243dbf8693596d6557051fcd9c26
David Weiß <davidweisspost@gmail.com>
2019-10-14 13:58:45 +0200

    Fix csv format

--- handle-hdt.py
+++ handle-hdt.py
@@ -3,20 +3,20 @@ from hdt import HDTDocument
 document = HDTDocument("swdf.hdt")
 
 # Display metadata
-print("nb triples: %i" % document.total_triples)
-print("nb subjects: %i" % document.nb_subjects)
-print("nb predicates: %i" % document.nb_predicates)
-print("nb objects: %i" % document.nb_objects)
-print("nb shared subject-object: %i" % document.nb_shared)
+print("unique triples: %i" % document.total_triples)
+print("unique subjects: %i" % document.nb_subjects)
+print("unique predicates: %i" % document.nb_predicates)
+print("unique objects: %i" % document.nb_objects)
+print("unique shared subject-object: %i" % document.nb_shared, "\n")
 
-# Fetch all triples that matches { ?s ?p ?o }
-(triples, cardinality) = document.search_triples("", "", "", limit=10, offset=10)
-
+# Query all triples that matches { ?s ?p ?o }
+triples, cardinality = document.search_triples("", "", "", limit=3, offset=10)
+print("query cardinality", cardinality)
 for triple in triples:
   print(triple)
 
 # Searching for triple IDs
-(triples, cardinality) = document.search_triples_ids(0, 0, 0, limit=10, offset=10)
+triples, cardinality = document.search_triples_ids(0, 0, 0, limit=3, offset=10)
 
 for s, p, o in triples:
   print(s, p, o) # will print 3-element tuples of integers
