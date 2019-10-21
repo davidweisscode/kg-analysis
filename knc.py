575686e719c761177c32965e0f9acb71ff5e20d8
david <davidweisspost@gmail.com>
2019-10-21 15:46:25 +0200

    Create graph from hdt query

--- knc.py
+++ knc.py
@@ -5,18 +5,43 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import time
 import numpy as np
+from hdt import HDTDocument
 
 start_time = time.time()
 
-CL_RED = "#e13232"
-CL_GREEN = "#32c832"
+# Constants
+RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+DBO = "http://dbpedia.org/ontology/"
+DBR = "http://dbpedia.org/resource/"
+RED = "#e13232"
+GREEN = "#32c832"
 
+# Query dataset
 inputfile = sys.argv[1]
-data = open(inputfile, "r")
-next(data, None)
-G = nx.parse_edgelist(data, delimiter=",", comments="mysupercomplicatedmarker")
-# TODO: Change the way a graph is build, From HDT file & query
+dataset = HDTDocument(inputfile)
+G = nx.Graph()
 
+musicians = []
+ka_musicians = []
+edge_list = []
+
+(triples, card) = dataset.search_triples("", RDF + "type", DBO + "MusicalArtist")
+for triple in triples:
+    musicians.append(triple[0])
+
+for musician in musicians:
+    (triples, card) = dataset.search_triples(musician, DBO + "birthPlace", DBR + "Karlsruhe")
+    for triple in triples:
+        ka_musicians.append(triple[0])
+
+for ka_musician in ka_musicians:
+    (triples, card) = dataset.search_triples(ka_musician, "", "")
+    for triple in triples:
+        edge_list.append((triple[0], triple[1]))
+
+G.add_edges_from(edge_list)
+
+# Analyze dataset
 print("G connected:", nx.is_connected(G))
 print("G bipartite:", nx.bipartite.is_bipartite(G))
 
@@ -29,9 +54,9 @@ coloring = nx.greedy_color(G)
 colorList = []
 for node in G.nodes():
     if coloring[node] == 0:
-        colorList.append(CL_RED)
+        colorList.append(RED)
     else:
-        colorList.append(CL_GREEN)
+        colorList.append(GREEN)
 
 bipartition_1, bipartition_2 = nx.bipartite.sets(G)
 if len(bipartition_1) > len(bipartition_2):
@@ -42,16 +67,18 @@ else:
 G_U = nx.algorithms.bipartite.projection.weighted_projected_graph(G, U)
 G_V = nx.algorithms.bipartite.projection.weighted_projected_graph(G, V)
 
-# Max k value of the graph to have at least one edge
+# TODO: Set up a blacklist to ignore generic properties which are not class specific
+
+# Max k value of the one mode graph to have edges
 k_max_U = G_V.number_of_nodes()
 k_max_V = G_U.number_of_nodes()
-
 print("k_max_U =", k_max_U)
 print("k_max_V =", k_max_V)
 
 G_U_1 = G_U.copy()
 G_V_1 = G_V.copy()
 
+# TODO: Use also other connectivity measures
 densitySum = 0
 knc_list_U = []
 for k in range(1, k_max_U + 1):
@@ -60,7 +87,6 @@ for k in range(1, k_max_U + 1):
             G_U.remove_edge(edge[0], edge[1])
     knc_list_U.append((k/k_max_U, nx.classes.function.density(G_U)))
     densitySum += nx.classes.function.density(G_U)
-
 RC_U = (1 / k_max_U) * densitySum
 print("RC_U =", RC_U)
 
@@ -72,7 +98,6 @@ for k in range(1, k_max_V + 1):
             G_V.remove_edge(edge[0], edge[1])
     knc_list_V.append((k/k_max_V, nx.classes.function.density(G_V)))
     densitySum += nx.classes.function.density(G_V)
-
 RC_V = (1 / k_max_V) * densitySum
 print("RC_V =", RC_V, "\n")
 
@@ -81,31 +106,31 @@ bipartiteLayout = nx.bipartite_layout(G, U, aspect_ratio=0.5, scale=0.2)
 nx.draw_networkx(G, bipartiteLayout, with_labels=True, font_size=6, node_color=colorList, edge_color="grey")
 
 plt.subplot(323, frameon=False) # One mode network G_U
-nx.draw_networkx(G_U_1, nx.circular_layout(G_U_1), with_labels=True, font_size=6, node_color=CL_RED, edge_color="grey")
+nx.draw_networkx(G_U_1, nx.circular_layout(G_U_1), with_labels=True, font_size=6, node_color=RED, edge_color="grey")
 nx.draw_networkx_edge_labels(G_U_1, nx.circular_layout(G_U_1), font_size=6)
 
 plt.subplot(324, frameon=False) # One mode network G_V
-nx.draw_networkx(G_V_1, nx.circular_layout(G_V_1), with_labels=False, node_color=CL_GREEN, edge_color="grey")
+nx.draw_networkx(G_V_1, nx.circular_layout(G_V_1), with_labels=False, node_color=GREEN, edge_color="grey")
 
 plt.subplot(322, frameon=False) # KNC plot
 # plt.plot(*zip(*knc_list_U), color="#ff0000")
 # plt.plot(*zip(*knc_list_V), color="#00ff00")
-plt.bar(list(zip(*knc_list_U))[0], list(zip(*knc_list_U))[1], width=-1/k_max_U, align="edge", color=CL_RED+"aa", edgecolor=CL_RED+"55")
-plt.bar(list(zip(*knc_list_V))[0], list(zip(*knc_list_V))[1], width=-1/k_max_V, align="edge", color=CL_GREEN+"aa", edgecolor=CL_GREEN+"55")
+plt.bar(list(zip(*knc_list_U))[0], list(zip(*knc_list_U))[1], width=-1/k_max_U, align="edge", color=RED+"aa", edgecolor=RED+"55")
+plt.bar(list(zip(*knc_list_V))[0], list(zip(*knc_list_V))[1], width=-1/k_max_V, align="edge", color=GREEN+"aa", edgecolor=GREEN+"55")
 
 G_U_adj = nx.to_numpy_matrix(G_U_1)
 G_U_adj[np.triu_indices_from(G_U_adj, 0)] = 0 # Set upper triangle to zeros
 ax_U = plt.subplot(325, frameon=False) # Adjacency matrix G_U as heatmap
 ax_U.set_xticks(np.arange(len(G_U_1.nodes())))
 ax_U.set_yticks(np.arange(len(G_U_1.nodes())))
-ax_U.set_xticklabels(G_U_1.nodes()) # TODO: Shorten node names, Only use from right side until first slash?
+ax_U.set_xticklabels(G_U_1.nodes()) # TODO: Shorten node names?
 ax_U.set_yticklabels(G_U_1.nodes())
 plt.setp(ax_U.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
 plt.imshow(G_U_adj, interpolation='nearest', cmap=plt.cm.Reds)
 plt.colorbar()
 
 G_V_adj = nx.to_numpy_matrix(G_V_1)
-G_V_adj[np.triu_indices_from(G_V_adj, 0)] = 0 # Set upper triangle to zeros
+G_V_adj[np.triu_indices_from(G_V_adj, 0)] = 0
 ax_V = plt.subplot(326, frameon=False) # Adjacency matrix G_V as heatmap
 ax_V.set_xticks(np.arange(len(G_V_1.nodes())))
 ax_V.set_yticks(np.arange(len(G_V_1.nodes())))
@@ -120,9 +145,9 @@ for row in range(0, k_max_U):
     for col in range(row + 1, k_max_U):
         pairs.append((G_V_adj.item(row, col), row, col))
 
-sorted_pairs = sorted(pairs, key=lambda x: x[0], reverse=True) # Use list[x] value as sort criterion
+sorted_pairs = sorted(pairs, key=lambda x: x[0], reverse=True) # Use pairs[0] as sort criterion
 
-for i in range(0, 700): # Print top 10 strongest pairs
+for i in range(0, 10): # Print strongest pairs
     pair = sorted_pairs[i]
     print(i + 1, pair[0], list(G_V_1.nodes)[pair[1]], list(G_V_1.nodes)[pair[2]])
 
--- query-hdt.py
+++ query-hdt.py
@@ -1,38 +1,52 @@
+#!/usr/bin/python3
+
+import networkx as nx
 from hdt import HDTDocument
 
-document = HDTDocument("hdt/swdf.hdt")
-# TODO: Use DBPedia 2016-04 English
+# SPARQL Prefixes
+rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+dbo = "http://dbpedia.org/ontology/"
+dbr = "http://dbpedia.org/resource/"
+
+document = HDTDocument("hdt/dbpedia2016-04en.hdt")
+
+G = nx.Graph()
 
 # Get metadata
-print("unique triples: %i" % document.total_triples)
-print("unique subjects: %i" % document.nb_subjects)
-print("unique predicates: %i" % document.nb_predicates)
-print("unique objects: %i" % document.nb_objects)
-print("unique shared subject-object: %i" % document.nb_shared, "\n")
+# print("unique triples: %i" % document.total_triples)
+# print("unique subjects: %i" % document.nb_subjects)
+# print("unique predicates: %i" % document.nb_predicates)
+# print("unique objects: %i" % document.nb_objects)
+# print("unique shared subject-object: %i" % document.nb_shared, "\n")
 
 # Query all triples that matches { ?s ?p ?o }
-triples, cardinality = document.search_triples("", "", "", limit=100, offset=10)
-print("query cardinality", cardinality)
-for triple in triples:
-  print(triple)
+# triples, cardinality = document.search_triples("", "", "", limit=10, offset=10)
+# print("query cardinality", cardinality)
+# for triple in triples:
+#     print(triple)
 
 # Option 1: Extract from mappings
-tp_a = ("?s", "http://swrc.ontoware.org/ontology#url", "?o")
-tp_b = ("?s", "?p", "http://dh2010.cch.kcl.ac.uk/academic-programme/abstracts/papers/pdf/ab-753.pdf")
-iterator = document.search_join([tp_a, tp_b])
-print("estimated join cardinality : %i" % len(iterator))
-for mapping in iterator:
-  print(mapping)
+# tp_a = ("?s", "http://swrc.ontoware.org/ontology#url", "?o")
+# tp_b = ("?s", "?p", "http://dh2010.cch.kcl.ac.uk/academic-programme/abstracts/papers/pdf/ab-753.pdf")
+# iterator = document.search_join([tp_a, tp_b])
+# print("estimated join cardinality : %i" % len(iterator))
+# for mapping in iterator:
+#   print(mapping)
 
 # Option 2: Sequential search for triples
 musicians = list()
 edge_list = list()
-(triples, card) = document.search_triples("?s", "rdf:type", "Musician")
+(triples, card) = document.search_triples("", rdf + "type", dbo + "MusicalArtist")
 for triple in triples:
-  musicians.append(triple[0])
+    musicians.append(triple[0])
+
 for musician in musicians:
-  (triples, card) = document.search_triples(musicians, "", "")
-  for triple in triples:
-      edge_list.append((musician, triple[1]))
+    (triples, card) = document.search_triples(musician, dbo + "birthPlace", dbr + "Karlsruhe", limit=10)
+    for triple in triples:
+        edge_list.append((musician, triple[1]))
+
+print(edge_list)
 
 # Construct Graph sequentially, Iterate over edge_list
+
+G.add_edges_from(edge_list)
