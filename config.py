717b10fcba818a18370507b02f8e9da99d056752
david <davidweisspost@gmail.com>
2019-11-14 21:37:00 +0100

    Add config file draft

---
+++ config.py
@@ -0,0 +1,8 @@
+config = {
+    "classes": ["Athlete", "Musician", "WrittenWork"],
+    "analyzeChildClasses": True,
+    "sampleSizeLimit": 100,
+    "kgSourcePath": "kg/dbpedia2016-04en.hdt",
+    "u": True,
+    "v": True,
+}
