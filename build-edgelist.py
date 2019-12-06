73d8587bb5b0ddcf6cf27197e279988251cfe028
David Weiß <davidweisspost@gmail.com>
2019-12-06 15:41:18 +0100

    Parse ontology only once

--- build-edgelist.py
+++ build-edgelist.py
@@ -24,7 +24,6 @@ def query_subclasses(superclass):
     # Option 1: Mappings from dataset.join()
     # Option 2: Sequential querying with pyHDT
     # Option 3: https://github.com/comunica/comunica-actor-init-sparql-hdt
-    ontology = Graph().parse(module.config["kg_ontology"])#TODO: Do this only once?
     subclass_query = f"""
     SELECT ?subclass
     WHERE 
@@ -36,6 +35,7 @@ def query_subclasses(superclass):
     results = ontology.query(subclass_query)
     for result in results:
         subclasses.append(str(result['subclass']))
+    #TODO: Remove "dpbedia URL" from subclass names ?
     return subclasses
 
 # Get edgelist for a superclass and all its subclasses
@@ -57,35 +57,38 @@ def get_subject_predicate_tuples(subclasses, subject_limit, predicate_limit):
 
 def write_edgelist(classname, edgelist):
     df = pd.DataFrame(edgelist, columns=["u", "v"])
-    df.to_csv("csv/" + classname + ".g.csv", index=False)#TODO: Create dir if required
+    df.to_csv("csv/" + classname + ".g.csv", index=False)
 
 def append_result_rows(superclass, subclasses, number_of_edges):
     df = pd.read_csv("csv/_results.csv")
     df.loc[len(df)] = (superclass, subclasses, number_of_edges)
     df.to_csv("csv/_results.csv", index=False)
 
-start_time = time.time()
+#TODO: Setup: Check, create and reset any missing dirs or files
+#             (csv, kg, _results.csv, csv/*.csv, config, log.txt)
 with open("blacklist.txt", "r") as file:
     BLACKLIST = file.read().splitlines()
 config_file = sys.argv[1]
 module = import_module(config_file)
-
 dataset = HDTDocument(module.config["kg_source"])
+t_ontology = time.time()
+ontology = Graph().parse(module.config["kg_ontology"])
+print("\n[Runtime] load-ontology %.3f sec" % (time.time() - t_ontology))
 subject_limit = module.config["subject_limit"]
 predicate_limit = module.config["predicate_limit"]
-
 if os.path.exists("csv/_results.csv"):
     print("Remove old results file")
     os.remove("csv/_results.csv")
-
 results = pd.DataFrame(columns=["superclass", "subclasses", "num_edges"])
 results.to_csv("csv/_results.csv", index=False)
+
+t_build = time.time()
 
 for superclass in module.config["classes"]:
     print("\n[Build edgelist]", superclass)
     subclasses = query_subclasses(superclass)
     edgelist = get_subject_predicate_tuples(subclasses, subject_limit, predicate_limit)
     write_edgelist(superclass, edgelist)
-    append_result_rows(superclass, subclasses, len(edgelist))#TODO: Remove "dpbedia URL" from subclass names
+    append_result_rows(superclass, subclasses, len(edgelist))
 
-print("\n[Runtime build-edgelist] %.3f sec" % (time.time() - start_time))
+print("\n[Runtime] build-edgelist %.3f sec" % (time.time() - t_build))
--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 config = {#TODO: Name for config file same as results file
-    "classes": ["Artist", "WrittenWork"],
-    "entail_subclasses": True,#TODO
-    "continue_process": False,#TODO
-    "kg_source": "kg/dbpedia2016-04en.hdt",
-    "kg_ontology": "kg/dbpedia.owl",
+    "classes": ["Artist", "Athlete", "WrittenWork", "Company", "Criminal", "Coach", "Writer"],
     "subject_limit": 10,
     "predicate_limit": 10,
+    "kg_source": "kg/dbpedia2016-04en.hdt",
+    "kg_ontology": "kg/dbpedia.owl",
+    "entail_subclasses": True,#TODO
+    "continue_process": False,#TODO
     "u": True,#TODO
     "v": True,#TODO
 }
