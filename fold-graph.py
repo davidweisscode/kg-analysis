8bb5960ea1b2a17bc6222b6c72bc94a6180b5f6f
David Weiß <davidweisspost@gmail.com>
2019-12-06 20:46:15 +0100

    Fold biadjacency matrix

--- fold-graph.py
+++ fold-graph.py
@@ -15,25 +15,20 @@ def read_edgelist(superclass):
     df = pd.read_csv("csv/" + superclass + ".g.csv")
     return list(df.itertuples(index=False, name=None))
 
-def fold_bipartite_graph(graph):
-    U = []
-    V = []
-    for edge in edgelist:
-        U.append(edge[0])
-        V.append(edge[1])
-    U = list(set(U))
-    V = list(set(V))
-    print("Compute one mode graph U")
-    G_U = nx.algorithms.bipartite.projection.weighted_projected_graph(graph, U)#TODO: RAM+SWAP at 100% after 40min, then "Killed"
-    print("Compute one mode graph V")
-    G_V = nx.algorithms.bipartite.projection.weighted_projected_graph(graph, V)
+def fold_bipartite_graph(bipgraph, U, V):
+    # TODO: Save row and column name order, Later name lookup in folded matrix F, Names for e.g. heatmap analysis
+    # TODO: fold the onemodes one at a time to reduce RAM
+    print("Compute biadjacency matrix A")
+    A = nx.bipartite.biadjacency_matrix(bipgraph, row_order=U, column_order=V)
+    print("A shape", A.shape)
+    print("Compute G_U")
+    G_U = np.dot(A , A.T)
+    print("G_U shape", G_U.shape)
+    # del G_U # Free up memory
+    print("Compute G_V")
+    G_V = np.dot(A.T , A)
+    print("G_V shape", G_V.shape)
     return G_U, G_V
-
-def get_k_max(onemode_graph): # TODO: Add error handling
-    if onemode_graph == G_U:
-        return int(G_V.number_of_nodes())
-    if onemode_graph == G_V:
-        return int(G_U.number_of_nodes())
 
 def append_result_columns(superclass, k_max_U, k_max_V, connected, bipartite):
     df = pd.read_csv("csv/_results.csv")
@@ -48,7 +43,7 @@ def write_edgelist(classname, edgelist, onemode):
     df = pd.DataFrame(edgelist, columns=[onemode + "_node_1", onemode + "_node_2", "weight"])
     df.to_csv("csv/" + classname + "." + onemode + ".csv", index=False)
 
-start_time = time.time()
+t_fold = time.time()
 config_file = sys.argv[1]
 module = import_module(config_file)
 
@@ -58,7 +53,16 @@ for superclass in module.config["classes"]:
     G = nx.Graph()
     edgelist = read_edgelist(superclass)
     G.add_edges_from(edgelist)
-    
+
+    is_connected = True
+    is_bipartite = True
+    if not nx.is_connected(G):
+        is_connected = False
+        print("[Info] Input graph is not connected", superclass)
+    if not nx.bipartite.is_bipartite(G):
+        is_bipartite = False
+        sys.exit("[Error] Input graph is not bipartite", superclass)
+
     U = []
     V = []
     for edge in edgelist:
@@ -67,33 +71,23 @@ for superclass in module.config["classes"]:
     U = list(set(U))
     V = list(set(V))
 
-    # Save row and column name order, Later name lookup in folded matrix F, Names for e.g. heatmap analysis
-    A = nx.bipartite.biadjacency_matrix(G, row_order=U, column_order=V)
-    print("A shape", A.shape)
-    F_1 = np.dot(A , A.T)
-    # del F_1 # Free up memory
-    print("F_1 shape", F_1.shape)
+    G_U, G_V = fold_bipartite_graph(G, U, V)
 
-    is_connected = True
-    is_bipartite = True
-    if not nx.is_connected(G):
-        is_connected = False
-        print("Info: Input graph is not connected", superclass)
-    if not nx.bipartite.is_bipartite(G):
-        is_bipartite = False
-        sys.exit("Error: Input graph is not bipartite", superclass)
+    k_max_U = G_V.shape[0]
+    k_max_V = G_U.shape[0]
+    print("k_max_U", k_max_U)
+    print("k_max_V", k_max_V)
 
-    G_U, G_V = fold_bipartite_graph(G)
+    print(G_U)
+    #TODO: Build G_U_edgelist [(art1, art2, w), (art1, art3, w), ...] from ndarray G_U
+    # Diagonal values of G_U represent to how much V nodes the U node is affiliated with
 
-    k_max_U = get_k_max(G_U)
-    k_max_V = get_k_max(G_V)
-
-    # In onemode network edgelists, data about disconnected nodes gets lost
     G_U_edgelist = list(G_U.edges.data("weight"))
     G_V_edgelist = list(G_V.edges.data("weight"))
     write_edgelist(superclass, G_U_edgelist, "u")
     write_edgelist(superclass, G_V_edgelist, "v")
 
+    # In onemode network edgelists, data about disconnected nodes gets lost
     append_result_columns(superclass, k_max_U, k_max_V, is_connected, is_bipartite)
 
-print("\n[Runtime fold-graph] %.3f sec" % (time.time() - start_time))
+print("\n[Runtime] fold-graph %.3f sec" % (time.time() - t_fold))
