1ba8c906dffa9a1145d45b3e7d40ddf5fbbcc5c8
David Weiß <davidweisspost@gmail.com>
2019-12-19 01:05:03 +0100

    Use unsigned int as matrix dtype, Improve refcycle

--- fold_graph.py
+++ fold_graph.py
@@ -53,23 +53,20 @@ def split_edgelist(edges):
 def fold_bipgraph(bipgraph, u, v):
     """ Fold a bipartite graph to its onemode representations using its biadjacency matrix """
     # TODO: Save row and column name order for later name lookup in wmatrix_u for e.g. heatmap analysis
-    # TODO: Fold the onemodes one at a time to reduce RAM
+    # TODO: Fold the onemodes one at a time to reduce space (increases time)
     # del wmatrix_u # Free up memory, Divide into two method calls?
     t_start = time.time()
-    A = nx.bipartite.biadjacency_matrix(bipgraph, row_order=u, column_order=v)
+    A = nx.bipartite.biadjacency_matrix(bipgraph, row_order=u, column_order=v, dtype="uint16")
     print("[Time] comp-biadj-matrix %.3f sec" % (time.time() - t_start))
     print("[Info] A shape", A.shape)
+    print("[Info] A dtype", A.dtype)
     t_start = time.time()
-    wmatrix_u = np.dot(A, A.T) # same as scipy.sparse.csr_matrix.dot ?
+    wmatrix_u = np.dot(A, A.T)
     print("[Time] onemode-dot-product U %.3f sec" % (time.time() - t_start))
     t_start = time.time()
     wmatrix_v = np.dot(A.T, A)
     print("[Time] onemode-dot-product V %.3f sec" % (time.time() - t_start))
     return wmatrix_u, wmatrix_v
-
-def tril_wmatrices(wmatrix_u, wmatrix_v):
-    """ Get the lower triangle of each of two matrices without the diagonal """
-    return sparse.tril(wmatrix_u, k=-1), sparse.tril(wmatrix_v, k=-1)
 
 def append_result_columns(superclass, k_max_u, k_max_v, connected, bipartite):
     """ Save more properties for each superclass in the result csv file """
@@ -100,28 +97,31 @@ for superclass in module.config["classes"]:
     # Diagonal values of wmatrix_u represent to how much v's the u is affiliated with
     #TODO: Save diagonal values in .csv for analysis of extensively described entities
     wmatrix_u, wmatrix_v = fold_bipgraph(bipgraph, side_u, side_v)
-    print("wmatrix_u type", type(wmatrix_u)) # scipy.sparse.csr.csr_matrix
-    print("wmatrix_u dtype", wmatrix_u.dtype) # int64
-    print("wmatrix_u nbytes", (wmatrix_u.data.nbytes + wmatrix_u.indptr.nbytes + wmatrix_u.indices.nbytes))
+    print("[Info] wmatrix_u type", type(wmatrix_u))
+    print("[Info] wmatrix_u dtype", wmatrix_u.dtype)
+    print("[Info] wmatrix_u nbytes in G", (wmatrix_u.data.nbytes + wmatrix_u.indptr.nbytes + wmatrix_u.indices.nbytes) / (1024 ** 3))
+
+    #TODO: Use dynamic dtypes (int16, uint16, ...) depending on wmatrix max values
+    print("[Info] wmatrix_u max", wmatrix_u.max())
+    print("[Info] wmatrix_v max", wmatrix_v.max())
+
+    # In onemode network edgelists, data about disconnected nodes gets lost
+    k_max_u = wmatrix_v.shape[0]
+    k_max_v = wmatrix_u.shape[0]
+    append_result_columns(superclass, k_max_u, k_max_v, is_connected, is_bipartite)
 
     # Fails here for large matrices
-    #TODO: Delete objects to free up RAM, Garbage collection: Use gc.collect()
-    #TODO: Change datatype of matrix elements from int64 to something else https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.astype.html
+    #TODO: Delete objects to free up RAM, Garbage collection
     #TODO: Change overcommit memory https://stackoverflow.com/questions/57507832/unable-to-allocate-array-with-shape-and-data-type
-    #TODO: Options https://www.quora.com/How-can-I-deal-with-the-memory-error-generated-by-large-Numpy-Python-arrays
-    wmatrix_u, wmatrix_v = tril_wmatrices(wmatrix_u, wmatrix_v)
-
+    #TODO: Change datatype of matrix elements from int64 to something else https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.astype.html
+    wmatrix_u = sparse.tril(wmatrix_u, k=-1)
     t_start = time.time()
     sparse.save_npz("out/" + superclass + ".u.npz", wmatrix_u)
     print("[Time] save-npz-u %.3f sec" % (time.time() - t_start))
+
+    wmatrix_v = sparse.tril(wmatrix_v, k=-1)
     t_start = time.time()
     sparse.save_npz("out/" + superclass + ".v.npz", wmatrix_v)
     print("[Time] save-npz-v %.3f sec" % (time.time() - t_start))
-
-    k_max_u = wmatrix_v.shape[0]
-    k_max_v = wmatrix_u.shape[0]
-
-    # In onemode network edgelists, data about disconnected nodes gets lost
-    append_result_columns(superclass, k_max_u, k_max_v, is_connected, is_bipartite)
 
 print("\n[Time] fold-graph %.3f sec" % (time.time() - t_fold))
