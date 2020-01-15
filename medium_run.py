bc20c05d3d34a1d119482dd49835cfd0bbcde939
David Weiss <davidweisspost@gmail.com>
2020-01-15 20:33:50 +0100

    Add run config examples

---
+++ medium_run.py
@@ -0,0 +1,7 @@
+config = {
+    "classes": ["Athlete", "Artist", "WrittenWork"],
+    "subject_limit": 100, # per subclass
+    "predicate_limit": 500, # per subject
+    "kg_source": "kg/dbpedia2016-04en.hdt",
+    "kg_ontology": "kg/dbpedia.owl",
+}
---
+++ small_run.py
@@ -0,0 +1,7 @@
+config = {
+    "classes": ["Athlete"],
+    "subject_limit": 10, # per subclass
+    "predicate_limit": 500, # per subject
+    "kg_source": "kg/dbpedia2016-04en.hdt",
+    "kg_ontology": "kg/dbpedia.owl",
+}
