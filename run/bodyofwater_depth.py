3268ad58b120b0c1b7475dcf539a2dc3c91d4cc7
David Weiss <davidweisspost@gmail.com>
2020-02-22 17:30:55 +0100

    Add runs

---
+++ run/bodyofwater_depth.py
@@ -0,0 +1,13 @@
+config = {
+    "classes": ["BodyOfWater",
+                "Lake",
+                "Stream",
+                "River",
+                "Canal"
+                ],
+    "project_method": "hyper",
+    "kg_source": "kg/dbpedia2016-04en.hdt",
+    "kg_ontology": "kg/dbpedia.owl",
+    "subject_limit": 0,
+    "predicate_limit": 0,
+}
---
+++ run/celestialbody_depth.py
@@ -0,0 +1,12 @@
+config = {
+    "classes": ["CelestialBody",
+                "Asteroid",
+                "Satellite",
+                "ArtificialSatellite"
+                ],
+    "project_method": "hyper",
+    "kg_source": "kg/dbpedia2016-04en.hdt",
+    "kg_ontology": "kg/dbpedia.owl",
+    "subject_limit": 0,
+    "predicate_limit": 0,
+}
