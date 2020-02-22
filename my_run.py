e48fc068cec7d0af75606211d1250c5f93a481ef
David Weiss <davidweisspost@gmail.com>
2020-02-22 15:50:18 +0100

    Ignore out

--- my_run.py
+++
@@ -1,8 +0,0 @@
-config = {
-    "classes": ["Athlete"],
-    "project_method": "hyper",
-    "kg_source": "kg/dbpedia2016-04en.hdt",
-    "kg_ontology": "kg/dbpedia.owl",
-    "subject_limit": 0,
-    "predicate_limit": 0,
-}
