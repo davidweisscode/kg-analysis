b383a6c586313e0941bad17826c98972456d3b05
David Weiß <updbk@aifb-ls3-vm4.aifb.kit.edu>
2020-02-24 12:41:34 +0100

    Add runs

---
+++ run/anatomicalstructure_breadth.py
@@ -0,0 +1,17 @@
+config = {
+    "classes": ["AnatomicalStructure",
+                "Artery",
+                "BloodVessel",
+                "Bone",
+                "Brain",
+		"Lymph",
+		"Muscle",
+		"Nerve",
+		"Vein"
+                ],
+    "project_method": "hyper",
+    "kg_source": "kg/dbpedia2016-04en.hdt",
+    "kg_ontology": "kg/dbpedia.owl",
+    "subject_limit": 0,
+    "predicate_limit": 0,
+}
---
+++ run/musicalwork_breadth.py
@@ -0,0 +1,16 @@
+config = {
+    "classes": ["MusicalWork",
+                "Album",
+                "Single",
+                "Song",
+                "Opera",
+		"Musical",
+		"NationalAnthem",
+		"ClassicalMusicComposition"
+                ],
+    "project_method": "hyper",
+    "kg_source": "kg/dbpedia2016-04en.hdt",
+    "kg_ontology": "kg/dbpedia.owl",
+    "subject_limit": 0,
+    "predicate_limit": 0,
+}
