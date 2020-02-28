d68afa8ebdec179c46deb2db00a2a66088c9a664
David Weiss <davidweisspost@gmail.com>
2020-02-28 22:36:55 +0100

    Compute knc metrics avg_dens, k_0, rel_rc

--- analyze_knc.py
+++ analyze_knc.py
@@ -1,5 +1,5 @@
 """
-Analyze various metrics of a KNC plot.
+Analyze a KNC function and compute metrics.
 """
 
 #!/usr/bin/python3
@@ -25,20 +25,26 @@ def analyze_knc(run_name, superclass):
     n_b = int(get_result(run_name, superclass, "n_b"))
 
     knc_t = read_knc_list(superclass, "t")
-    rc_t_dens, rc_t_ncomp, rc_t_slcc = analyze_knc_onemode(run_name, superclass, knc_t, n_t, n_b)
+    rc_t_dens, rc_t_ncomp, rc_t_slcc = compute_rc(knc_t, n_t, n_b)
+    avg_dens_t = compute_avg_dens(knc_t, n_b)
+    max_dens_t = knc_t[0][1]
+    k_0_t = get_k_0(knc_t)
+    rel_rc_t = get_rel_rc(run_name, superclass, rc_t_dens, "t")
 
     knc_b = read_knc_list(superclass, "b")
-    rc_b_dens, rc_b_ncomp, rc_b_slcc = analyze_knc_onemode(run_name, superclass, knc_b, n_b, n_t)
+    rc_b_dens, rc_b_ncomp, rc_b_slcc = compute_rc(knc_b, n_b, n_t)
+    avg_dens_b = compute_avg_dens(knc_b, n_t)
+    max_dens_b = knc_b[0][1]
+    k_0_b = get_k_0(knc_b)
+    rel_rc_b = get_rel_rc(run_name, superclass, rc_b_dens, "b")
 
     add_results(run_name, superclass,
                 rc_t_dens=rc_t_dens, rc_t_ncomp=rc_t_ncomp, rc_t_slcc=rc_t_slcc,
-                rc_b_dens=rc_b_dens, rc_b_ncomp=rc_b_ncomp, rc_b_slcc=rc_b_slcc)
+                avg_dens_t=avg_dens_t, max_dens_t=max_dens_t, k_0_t=k_0_t, rel_rc_t=rel_rc_t,
+                rc_b_dens=rc_b_dens, rc_b_ncomp=rc_b_ncomp, rc_b_slcc=rc_b_slcc,
+                avg_dens_b=avg_dens_b, max_dens_b=max_dens_b, k_0_b=k_0_b, rel_rc_b=rel_rc_b,)
 
-def analyze_knc_onemode(run_name, superclass, knc_list, n_max, k_max):
-    #TODO: lower_quartile, median, upper_quartile, slope
-    return compute_rc(run_name, superclass, knc_list, n_max, k_max)
-
-def compute_rc(run_name, superclass, knc_list, n_max, k_max):
+def compute_rc(knc_list, n_max, k_max):
     """ Compute representational consistency (AUC of KNC) based on connectivity measure """
     density_sum = 0
     if len(knc_list[0]) == 4: # Graph was build to compute connectivity measures
@@ -61,6 +67,29 @@ def compute_rc(run_name, superclass, knc_list, n_max, k_max):
         rc_density = (1 / k_max) * density_sum
         print(f"[Info] rc_density {rc_density:.8f}")
         return rc_density, None, None
+
+def compute_avg_dens(knc_list, k_max):
+    """ Compute the average density of a knc function with density connectivity measure """
+    density_sum = 0
+    for knc_step in knc_list:
+        density_sum = density_sum + knc_step[1]
+    return density_sum / k_max
+
+def get_k_0(knc_list):
+    """ Return the k at which density becomes zero, Return 0 if density never becomes zero """
+    for knc_step in knc_list:
+        if knc_step[1] == 0:
+            return knc_step[0]
+    return 0
+
+def get_rel_rc(run_name, superclass, rc_dens, onemode):
+    """ Compute the representational consistency relative to its superclass """
+    df = pd.read_csv(f"out/_results_{run_name}.csv", index_col=0)
+    if onemode == "t":
+        rc_dens_super = df.iloc[0].rc_t_dens
+    elif onemode == "b":
+        rc_dens_super = df.iloc[0].rc_b_dens
+    return rc_dens / rc_dens_super
 
 def add_results(run_name, superclass, **results):
     """ Append result columns in a superclass row """
