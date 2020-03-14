ef71eef9f0684fe511a2c46c47f1a27e282b1329
David Weiss <davidweisspost@gmail.com>
2020-03-14 10:47:13 +0100

    Sample dblp documents

--- handle_dblp.py
+++ handle_dblp.py
@@ -1,31 +1,34 @@
-# Create classes from dblp document dataset
-# Split into journals, conf, ...
-# Get edgelists
+# Get edgelist from dblp document dataset
 
 import pandas as pd
+from tqdm import tqdm
 
 def write_edgelist(classname, edgelist):
     """ Write edgelist to csv file """
     df = pd.DataFrame(edgelist, columns=["t", "b"])
-    df.to_csv(f"out/{classname}/{classname}.g.csv", index=False)
+    df.to_csv(f"out/DocumentDBLP/{classname}.g.csv", index=False)
 
-df = pd.read_csv(f"out/JournalDBLP/DocumentDBLP.g.csv")
-print("len Document", len(df))
+df = pd.read_csv(f"out/DocumentDBLP/DocumentDBLP.g.csv")
 
+# print("len Document", len(df))
 # for blacklisted_predicate in blacklist:
 #     df = df[df["b"] != blacklisted_predicate]
-
-conferences = df["t"].str.contains("/conf/")
-df = df[~conferences]
-print("len Journal", len(df))
 
 print("unique n_t", df["t"].nunique())
 print("unique n_b", df["b"].nunique())
 
-edgelist = list(df.itertuples(index=False, name=None))
+# print(df["b"].unique()[:3])
 
+# Sample subset of same size as WrittenWork in DBpedia (n_t 90862)
+df_samples = pd.DataFrame(columns=["t", "b"])
+subj_samples = df["t"].unique()[:90862] #[:90862]
+for index, row in tqdm(df.iterrows(), total=len(df)):
+    if row["t"] in subj_samples:
+        df_samples.loc[index] = row
+
+print("unique samples n_t", df_samples["t"].nunique())
+print("unique samples n_b", df_samples["b"].nunique())
+
+edgelist = list(df_samples.itertuples(index=False, name=None))
 print("example edges\n", edgelist[:10])
-
-# TODO: How to sample subset of same size as AcademicJournal in DBpedia (n_t 7688) ?
-
-write_edgelist("JournalDBLP", edgelist)
+write_edgelist("SampleDocumentDBLP", edgelist)
