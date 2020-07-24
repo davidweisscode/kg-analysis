5aaf7239ffa87a55101d05066e96b2bfc7366ff9
David Weiss <davidweisspost@gmail.com>
2020-07-24 16:18:41 +0200

    Extract wikidata classes

--- run/mixed.py
+++ run/mixed.py
@@ -1,10 +1,9 @@
 config = {
     "classes": [
-                "BoxerWikidata",
-                "CyclistWikidata",
+                "Mixed",
                 ],
     "project_method": "hyper",
-    "kg_source": "kg/wikidata-20170313-all-BETA.hdt",
+    "kg_source": None,
     "kg_ontology": None,
     "subject_limit": 0,
     "predicate_limit": 0,
